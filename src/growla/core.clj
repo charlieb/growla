8f53fe01685a1dbae973cde77656523ce569b83b
Charlie Burrows <charlie.burrows@asml.com>
2019-07-17 14:21:30 -0400

    WIP distance based types

--- src/growla/core.clj
+++ src/growla/core.clj
@@ -17,7 +17,8 @@
 (def pentagon-points (map angle-to-vec pentagon-angles))
 (def hexagon-points  (map angle-to-vec hexagon-angles))
 
-(defn seeds [n w h]
+;;;;;;;;;;;;;; SEEDS ;;;;;;;;;;;;
+(defn seeds-kind [n w h]
   (map (fn [_]
          (let [kind-offset 
                ; (rand-nth pentagon-angles)
@@ -30,14 +31,43 @@
                       (map #(angle-to-vec (+ % kind-offset)) hexagon-angles))))
        (range n)))
 
+(defn seeds-numeric-kind [n w h]
+  (map #(mk-circle (q/random (- (/ w 2)) (/ w 2))
+                   (q/random (- (/ h 2)) (/ h 2))
+                   radius
+                   nil
+                   %)
+       (range n))
+  [(mk-circle 0. 0. radius nil 0)
+   (mk-circle 5. 0. radius nil 1)
+   (mk-circle 5. 5. radius nil 2)
+   (mk-circle 0. 5. radius nil 3)]
+  )
+
+(defn seeds-center [_ _ _] [(mk-circle 0. 0. radius nil nil)])
+(def seeds seeds-kind)
+
+;;;;;;;;;;;;;; SEEDS ;;;;;;;;;;;;
+
 (defn init-growla []
-  (let [c0 (mk-circle 10. 10. radius nil 'pentagonal)
-        c1 (mk-circle -10. -10. radius nil 'hexagonal)
+  (let [
         cs (seeds 50 (/ (q/width) 2) (/ (q/height) 2)) ; [c0 c1]
         tree (kd/build-tree (vec (map #(with-meta (to-v2 %) %) cs)))
         ]
     {:circles cs :tree tree}
     ))
+
+;;;;;;;;;;;; GROW ;;;;;;;;;;;;;;
+(defn grow-kind2 [cs tree]
+  (let [candidate (rand-nth cs)
+        pos (to-v2 candidate)
+        new-pos (vec (map #(+ %1 (* 2 radius %2)) pos (q/random-2d)))
+        nears (kd/nearest-neighbor tree new-pos 20)]
+    ;(println (:kind candidate))
+    ;(doseq [near nears](println "   " (:kind (meta near))))
+    (when (and (= pos (:point (first nears)))
+               (some #(not (= (:kind candidate) (:kind (meta %)))) nears))
+        (mk-circle (new-pos 0) (new-pos 1) radius candidate (:kind candidate)))))
 
 (defn grow-kind [cs tree]
   (let [candidate (rand-nth cs)
@@ -47,22 +77,28 @@
     (when (= pos (:point (kd/nearest-neighbor tree new-pos)))
       (mk-circle (new-pos 0) (new-pos 1) radius candidate (:kind candidate)))))
 
-(defn grow [cs tree]
+(defn grow-random [cs tree]
   (let [candidate (rand-nth cs)
         pos (to-v2 candidate)
         new-pos (vec (map #(+ %1 (* 2 radius %2)) pos (q/random-2d)))]
     (when (= pos (:point (kd/nearest-neighbor tree new-pos)))
-      (mk-circle (new-pos 0) (new-pos 1) radius candidate))))
+      (mk-circle (new-pos 0) (new-pos 1) radius candidate (:kind candidate)))))
+
+;;;;;;;;;; CONFIG ;;;;;;;;;;
+(def grow grow-kind)
+;;;;;;;;;;;; ;;;;;;;;;;;;;;
 
 (defn growtimes [state n]
   (loop [cs (:circles state)
          tree (:tree state)
-         n n]
-    (if (zero? n)
+         n n
+         failures 0]
+    (println n)
+    (if (or (> failures 1000) (zero? n))
       {:circles cs :tree tree}
-      (if-let [c (grow-kind cs tree)]
-        (recur (conj cs c) (kd/insert tree (with-meta (to-v2 c) c)) (dec n))
-        (recur cs tree n)))))
+      (if-let [c (grow cs tree)]
+        (recur (conj cs c) (kd/insert tree (with-meta (to-v2 c) c)) (dec n) 0)
+        (recur cs tree n (inc failures))))))
 
 ;;;;;;;;;;;;;;;;;;; ;;;;;;;;;;;;;;;;;;;;
 (defn setup []
@@ -70,7 +106,7 @@
   (q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :rgb)
-  (growtimes (init-growla) 4000))
+  (growtimes (init-growla) 1000))
 
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
